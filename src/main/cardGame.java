0f72b6c24d24fb0fb9aed11989c4b4283a7d0e7a
Julian Lung <julianlungszelin2001@gmail.com>
2022-11-04 11:34:06 +0000

    Update cardGame.java
    fixed some log file dupe problem, added rotation method (not finished) for going around the table. added check winner method. added some sort of run to start player threads

--- src/main/cardGame.java
+++ src/main/cardGame.java
@@ -38,29 +38,46 @@ class CardGame {
         }
     }
 
+
+
+
+    void run() {
+        System.out.println("Begin game:");
+
+        ArrayList<Thread> threads = new ArrayList<>();
+        for (Player player : players) {
+            Thread t = new Thread(player);
+            t.start();
+            threads.add(t);
+        }
+
+        // wait for all threads to finish
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Winner: " + getWinner());
+        System.out.println("Game over.");
+    }
+
     //player, plays as thread
     class Player implements Runnable {
         private final int playerId;
+        private ArrayList<Integer> card;
 
         Player(int playerId, ArrayList<Integer> card) {
             this.playerId = playerId;
             this.card = card;
 
             create_log_file();
-        }
-
-        ArrayList<Integer> getCard() {
-            return card;
-        }
-
-        //winner check
-        public Player getWinner() {
-
-            return winners.get(0);
+        
         }
         //log file baffoonery
         private String create_log_file() {
-            String output = "player" + this.playerId + "_output.txt";
+            String output = "Player" + playerId + "_output.txt";
             File log_file = new File(output);
             try {
               if (log_file.exists())
@@ -68,38 +85,34 @@ class CardGame {
               log_file.createNewFile();
               return output;
             } catch (IOException e) {
-              System.out.println("Couldnt create file " + output);
+              System.out.println("Couldnt create file: " + output);
               return null;}
               
-            } 
+            }
+        
+
+        ArrayList<Integer> getCard() {
+            return card;
+        }
+    }
 
 
-            private void rotations() {
-                synchronized (Player.class) {
-                    if (winner) {
-                        return;
-                    }
+    //winner check
+    public Player getWinner() {
 
+        return winners.get(0);
+        }
+      
+              
+         
 
-
-            return winners.get(0);
-
-            //log file thing
-            private String create_log_file () {
-                String path = "player" + this.playerId + "_output.txt";
-                File log_file = new File(path);
-                try {
-                    if (log_file.exists())
-                        log_file.delete();
-                    log_file.createNewFile();
-                    return path;
-                } catch (IOException e) {
-                    System.out.println("Warning: could not create " + path);
-                    return null;
-                }
-
+    //go around the table
+    private void rotations() {
+        synchronized (Player.class) {
+            if (winner) {
+                return;
             }
         }
     }
-}
 
+}
