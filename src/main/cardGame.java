cc917e609f3d48ed2d79084bc1ea2e14a0bc03f7
Julian Lung <julianlungszelin2001@gmail.com>
2022-11-04 11:22:41 +0000

    add run and updates to player classs
    yaya

--- src/main/cardGame.java
+++ src/main/cardGame.java
@@ -37,40 +37,10 @@ class CardGame {
 
         }
     }
-    
 
-
-    //run player threads
-    void run() {
-        System.out.println("Game begins:");
-
-        ArrayList<Thread> threads = new ArrayList<>();
-        for (Player player : players) {
-            Thread t = new Thread(player);
-            t.start();
-            threads.add(t);
-        }
-
-        // wait for threads
-        for (Thread thread : threads) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        System.out.println("Winner: " + getWinner());
-        System.out.println("Game over");
-
-    }
-
-
-
-
-    //player thread thing
+    //player, plays as thread
     class Player implements Runnable {
         private final int playerId;
-        private ArrayList<Integer> card;
 
         Player(int playerId, ArrayList<Integer> card) {
             this.playerId = playerId;
@@ -82,39 +52,53 @@ class CardGame {
         ArrayList<Integer> getCard() {
             return card;
         }
-    }
 
-    public Player getWinner() {
-        return winners.get(0);
-    }
+        //winner check
+        public Player getWinner() {
 
-
-     //log file maker
-    private String create_log_file() {
-        String output = "Player" + playerId + "_output.txt";
-        File log_file = new File(output);
-        try {
-            if (log_file.exists())
-            log_file.delete(); 
-            log_file.createNewFile();
-             return output;
-        } catch (IOException e) {
-            System.out.println("Couldnt create file " + output);
-            return null;}
+            return winners.get(0);
+        }
+        //log file baffoonery
+        private String create_log_file() {
+            String output = "player" + this.playerId + "_output.txt";
+            File log_file = new File(output);
+            try {
+              if (log_file.exists())
+                log_file.delete(); 
+              log_file.createNewFile();
+              return output;
+            } catch (IOException e) {
+              System.out.println("Couldnt create file " + output);
+              return null;}
               
             } 
 
 
-    private void rotations() {
-        synchronized (Player.class) {
-            if (winner) {
+            private void rotations() {
+                synchronized (Player.class) {
+                    if (winner) {
                         return;
                     }
 
 
 
+            return winners.get(0);
 
-        
+            //log file thing
+            private String create_log_file () {
+                String path = "player" + this.playerId + "_output.txt";
+                File log_file = new File(path);
+                try {
+                    if (log_file.exists())
+                        log_file.delete();
+                    log_file.createNewFile();
+                    return path;
+                } catch (IOException e) {
+                    System.out.println("Warning: could not create " + path);
+                    return null;
+                }
+
+            }
         }
     }
 }
